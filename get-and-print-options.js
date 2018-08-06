0897305272a2e6b0c98aba345c90c4e04721931d
James Dowell <james.s.dowell@gmail.com>
2018-08-06 17:52:31 +0000

    added ability to take host and path values as parameters

---
+++ get-and-print-options.js
@@ -0,0 +1,26 @@
+function getAndPrintHTML (options) {
+  var https = require('https');
+  var str = '';
+
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      str += data;
+    });
+
+    response.on('end', function() {
+      console.log(str);
+    });
+
+  });
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
